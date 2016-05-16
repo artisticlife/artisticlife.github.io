84b4705149cf86d6382df8de3270f9d2b72a2052
artisticlife <artisticlifephoto@gmail.com>
2016-05-16 01:19:33 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 // Main javascript file
+//Smooth scrolling to anchors
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -12,4 +13,24 @@ $(function() {
       }
     }
   });
+});
+
+//Scroll to top
+$(document).ready(function(){
+	
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+	
 });
