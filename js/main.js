0acdd9975bc81d4ac167478c2b33f68ac9c4192a
artisticlife <artisticlifephoto@gmail.com>
2016-06-06 04:23:51 -0700

    Trying a different smoothScroll

--- js/main.js
+++ js/main.js
@@ -1,36 +1,33 @@
-// Main javascript file
-//Smooth scrolling to anchors
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top -180
-        }, 1000);
-        return false;
-      }
+// /**
+ * Check a href for an anchor. If exists, and in document, scroll to it.
+ * If href argument ommited, assumes context (this) is HTML Element,
+ * which will be the case when invoked by jQuery after an event
+ */
+function scroll_if_anchor(href) {
+    href = typeof(href) == "string" ? href : $(this).attr("href");
+    
+    // You could easily calculate this dynamically if you prefer
+    var fromTop = 180;
+    
+    // If our Href points to a valid, non-empty anchor, and is on the same page (e.g. #foo)
+    // Legacy jQuery and IE7 may have issues: http://stackoverflow.com/q/1593174
+    if(href.indexOf("#") == 0) {
+        var $target = $(href);
+        
+        // Older browser without pushState might flicker here, as they momentarily
+        // jump to the wrong position (IE < 10)
+        if($target.length) {
+            $('html, body').animate({ scrollTop: $target.offset().top - fromTop });
+            if(history && "pushState" in history) {
+                history.pushState({}, document.title, window.location.pathname + href);
+                return false;
+            }
+        }
     }
-  });
-});
+}    
 
-//Scroll to top
-$(document).ready(function(){
-	
-	//Check to see if the window is top if not then display button
-	$(window).scroll(function(){
-		if ($(this).scrollTop() > 100) {
-			$('.scrollToTop').fadeIn();
-		} else {
-			$('.scrollToTop').fadeOut();
-		}
-	});
-	
-	//Click event to scroll to top
-	$('.scrollToTop').click(function(){
-		$('html, body').animate({scrollTop : 0},800);
-		return false;
-	});
-	
-});
+// When our page loads, check to see if it contains and anchor
+scroll_if_anchor(window.location.hash);
+
+// Intercept all anchor clicks
+$("body").on("click", "a", scroll_if_anchor);
