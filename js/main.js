9ac62a6011793cc928e02ba79a8c1608ee4e1403
artisticlife <artisticlifephoto@gmail.com>
2016-06-06 04:35:06 -0700

    Added scroll to top back

--- js/main.js
+++ js/main.js
@@ -30,3 +30,23 @@ scroll_if_anchor(window.location.hash);
 
 // Intercept all anchor clicks
 $("body").on("click", "a", scroll_if_anchor);
+
+//Scroll to top
+ $(document).ready(function(){
+ 	
+ 	//Check to see if the window is top if not then display button
+ 	$(window).scroll(function(){
+ 		if ($(this).scrollTop() > 100) {
+ 			$('.scrollToTop').fadeIn();
+ 		} else {
+ 			$('.scrollToTop').fadeOut();
+ 		}
+ 	});
+ 	
+ 	//Click event to scroll to top
+ 	$('.scrollToTop').click(function(){
+ 		$('html, body').animate({scrollTop : 0},800);
+ 		return false;
+ 	});
+ 	
+ });
